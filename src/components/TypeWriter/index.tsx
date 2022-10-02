96d3795cf7c27aa7541ad15f5d8e32d6f299045c
Shanks <chenjinhuo@bytedance.com>
2022-10-02 17:30:28 +0800

    feat: complete home page

--- src/components/TypeWriter/index.tsx
+++ src/components/TypeWriter/index.tsx
@@ -1,8 +1,49 @@
+import './index.css'
+import typed from 'typed.js'
+import { useEffect, useRef } from 'react'
+const TypedCursorClassName = 'typed-cursor'
 export default function TypeWriter() {
+  const nameTypedDom = useRef<HTMLDivElement>(null)
+  const descriptionTypedDom = useRef<HTMLDivElement>(null)
+  const startDescriptionTyped = () => {
+    if (descriptionTypedDom.current) {
+      new typed(descriptionTypedDom.current, {
+        strings: ['I like to design SDK architecture and really into new tech things. ^1000 Focusing on Monitoring of Web and Mini Program recently.'],
+        typeSpeed: 30,
+        loop: false,
+      })
+      setTimeout(() => {
+        const lastElement = descriptionTypedDom.current?.parentElement?.lastElementChild
+        lastElement && lastElement?.classList.contains(TypedCursorClassName) && descriptionTypedDom.current?.parentElement?.removeChild(lastElement)
+      }, 8000)
+    }
+  }
+  useEffect(() => {
+    if (nameTypedDom.current) {
+      new typed(nameTypedDom.current, {
+        strings: ["Hey, I'm cjinhuo.", "Hey, I'm Shanks."],
+        typeSpeed: 50,
+        backSpeed: 36,
+        smartBackspace: true,
+        loop: false,
+        onStop(arrayPos, self) {
+          console.log('arrayPos, self', arrayPos, self)
+        },
+      })
+      setTimeout(() => {
+        const lastElement = nameTypedDom.current?.parentElement?.lastElementChild
+        lastElement && lastElement?.classList.contains(TypedCursorClassName) && nameTypedDom.current?.parentElement?.removeChild(lastElement)
+        startDescriptionTyped()
+      }, 4000)
+    }
+  }, [nameTypedDom])
   return (
-    <div>
-      <div className='text-skin-neutral-1'>
-        Hi,I'm Shanks. I like to design SDK architecture and really info new fun tech things. focus on Monitoring of Web & Mini Program recently
+    <div style={{ height: '100%', width: '100%' }}>
+      <div className='h-10'>
+        <div className='inline w-auto text-skin-neutral-1 text-2xl font-semibold antialiased typed-name' ref={nameTypedDom}></div>
+      </div>
+      <div>
+        <div className='inline w-auto text-skin-neutral-1' ref={descriptionTypedDom}></div>
       </div>
     </div>
   )
