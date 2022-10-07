f79b4ce0c88b0b6e62d3609aa82e756c4f5bd311
Shanks <chenjinhuo@bytedance.com>
2022-10-07 18:06:03 +0800

    feat: new favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
-});
+  site: 'https://cjinhuo.netlify.app/',
+  outDir: 'doc',
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+})
