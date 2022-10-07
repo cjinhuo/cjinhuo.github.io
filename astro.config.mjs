06a49da2ef3a2661fe3e08048ab496c4b358acd6
Shanks <chenjinhuo@bytedance.com>
2022-10-07 18:43:29 +0800

    ci(github): change output dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,5 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cjinhuo.netlify.app/',
-  outDir: 'doc',
   integrations: [mdx(), sitemap(), tailwind(), react()],
 })
