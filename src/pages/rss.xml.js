74f05d9db30b83cf65b2e378f1e355b8cdd6d5ed
陈金伙 <chenjinhuo@bytedance.com>
2023-09-28 15:00:56 +0800

    feat(app): :sparkles: upgrade to latest version

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,13 @@
-import rss from '@astrojs/rss'
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config'
 
-export const get = () =>
-  rss({
+export async function GET(context) {
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
-    items: import.meta.glob('./blogs/**/*.{md,mdx}'),
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./blogs/**/*.{md,mdx}')),
+    customData: '<language>en-us</language>',
   })
+}
