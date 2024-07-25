0c28fa8a7de9c4f2a41da031c57bed9ec8b87277
陈金伙 <chenjinhuo@bytedance.com>
2024-07-25 17:48:15 +0800

    style(app): :lipstick: add css duration

--- src/components/Header/NavLink.tsx
+++ src/components/Header/NavLink.tsx
@@ -21,19 +21,19 @@ interface TypeProps {
 export default function NavLink({ pathname }: TypeProps) {
   return (
     <>
-      {NavList.map(item => {
+      {NavList.map((item) => {
         const isActive = pathname === item.href
         const textClassName = isActive ? 'text-skin-neutral-1' : 'text-skin-neutral-5'
         let label
         if (item.label === 'Github') {
-          label = <IconPark className='hover:text-skin-neutral-1 transition-colors' name={NameTypes.github}></IconPark>
+          label = <IconPark className='hover:text-skin-neutral-1' name={NameTypes.github}></IconPark>
         }
         return (
           <a
             key={item.href}
             target={item.isBlank ? '_blank' : '_self'}
             href={item.href}
-            className={`hover:text-skin-neutral-1 transition-colors text-base ${textClassName}`}>
+            className={`hover:text-skin-neutral-1 transition duration-75 text-base ${textClassName}`}>
             {label || item.label}
           </a>
         )
