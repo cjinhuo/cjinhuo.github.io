5c98af43f5ede67f25f3e769a1be673e3be3b888
Shanks <cjinhuo@qq.com>
2024-08-18 21:52:23 +0800

    chore(app): :hammer: add my-projects.md

--- src/components/TypeWriter/index.tsx
+++ src/components/TypeWriter/index.tsx
@@ -1,6 +1,6 @@
-import './index.css'
-import typed from 'typed.js'
 import { useEffect, useRef } from 'react'
+import typed from 'typed.js'
+import './index.css'
 const TypedCursorClassName = 'typed-cursor'
 export default function TypeWriter() {
   const nameTypedDom = useRef<HTMLDivElement>(null)
@@ -8,13 +8,17 @@ export default function TypeWriter() {
   const startDescriptionTyped = () => {
     if (descriptionTypedDom.current) {
       new typed(descriptionTypedDom.current, {
-        strings: ['I like designing SDK architecture and really into new tech things. ^1000 Now focusing on Monitoring of Web and Mini Program at Bytedance.'],
+        strings: [
+          'I enjoy designing SDK architecture and really into new tech things. ^1000 Now focusing on Monitoring of Web、Mini Program and Node.js at Bytedance.',
+        ],
         typeSpeed: 30,
         loop: false,
       })
       setTimeout(() => {
         const lastElement = descriptionTypedDom.current?.parentElement?.lastElementChild
-        lastElement && lastElement?.classList.contains(TypedCursorClassName) && descriptionTypedDom.current?.parentElement?.removeChild(lastElement)
+        lastElement &&
+          lastElement?.classList.contains(TypedCursorClassName) &&
+          descriptionTypedDom.current?.parentElement?.removeChild(lastElement)
       }, 8000)
     }
   }
@@ -29,7 +33,9 @@ export default function TypeWriter() {
       })
       setTimeout(() => {
         const lastElement = nameTypedDom.current?.parentElement?.lastElementChild
-        lastElement && lastElement?.classList.contains(TypedCursorClassName) && nameTypedDom.current?.parentElement?.removeChild(lastElement)
+        lastElement &&
+          lastElement?.classList.contains(TypedCursorClassName) &&
+          nameTypedDom.current?.parentElement?.removeChild(lastElement)
         startDescriptionTyped()
       }, 4000)
     }
@@ -37,7 +43,9 @@ export default function TypeWriter() {
   return (
     <div style={{ height: '100%', width: '100%' }}>
       <div>
-        <div className='inline w-auto text-skin-neutral-1 text-2xl font-semibold antialiased typed-name' ref={nameTypedDom}></div>
+        <div
+          className='inline w-auto text-skin-neutral-1 text-2xl font-semibold antialiased typed-name'
+          ref={nameTypedDom}></div>
       </div>
       <div>
         <div className='inline w-auto text-skin-neutral-4' ref={descriptionTypedDom}></div>
---
+++ src/pages/blogs/2024/my-projects.md
@@ -0,0 +1,33 @@
+---
+layout: "../../../layouts/BlogPost.astro"
+title: "A lot of tools I created"
+description: "Here are some tiny tools I still maintained: 🎉"
+pubDate: '2024-08-18'
+updatedDate: '2024-08-18'
+author: 'Shanks'
+authorHref: 'https://github.com/cjinhuo'
+heroImage: '/placeholder-hero.jpg'
+heroImageAlt: 'heroImage'
+---
+
+Here are some tiny tools I still maintained: 🎉
+
+## @mitojs
+🎉 A lite SDK for monitoring Web and MiniProgram,[SDK Demo](https://mitojs.github.io/react-sdk-demo/#/page-one) for that,[Admin Demo](https://mitojs.github.io/mito-admin-demo/#/errors/2/info) for that,[Document](https://mitojs.github.io/mito-doc/#/sdk/guide/introduction) for that
+
+
+## morse
+📟 A [interesting toy](https://cjinhuo.github.io/morse/) of Morse code which could visualize Morse code in binary tree to train you memorize those codes easily and deeply.
+
+## text search engine
+🔍 A [text search engine](https://github.com/cjinhuo/text-search-engine) that relies on a dynamic programming algorithm and supports fuzzy search with pinyin.
+
+## blazwitcher
+⚒️ A [Chrome Extension](https://chrome.google.com/webstore/detail/fjgablnemienkegdnbihhemebmmonihg) for searching tabs, bookmarks, and history with support for fuzzy Pinyin search which let you switch or create tab you want in blazing speed.
+
+## parser
+⌨ A tiny parser from [parsing](https://cjinhuo.github.io/thrift-to-json-schema/) thrift to JSON schema
+
+
+## the best mono repo boilerplate
+📦 A set of scripts are preset for [monorepo](https://github.com/cjinhuo/mono-sdk-boilerplate) with Pnpm + changeset + nx
