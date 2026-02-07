92373437efade893fc74d6b0c1f63d46a86fe26a
Shanks <cjinhuo@qq.com>
2026-02-07 16:36:28 +0800

    chore(app): 🔨 update editorconfig with multi-language support

--- src/components/IconPark.tsx
+++ src/components/IconPark.tsx
@@ -1,24 +1,21 @@
 export enum NameTypes {
-  Light = 'light',
-  Dark = 'dark',
-  Github = 'github',
-  Resume = 'resume',
-  WeChat = 'wechat',
-  Email = 'email',
-  Skill = 'skill',
-  WorkExperience = 'work-experience',
-  PersonalProfile = 'personal-profile',
+	Light = 'light',
+	Dark = 'dark',
+	Github = 'github',
+	Resume = 'resume',
+	WeChat = 'wechat',
+	Email = 'email',
+	Skill = 'skill',
+	WorkExperience = 'work-experience',
+	PersonalProfile = 'personal-profile',
 }
 interface PropsType extends React.HTMLAttributes<any> {
-  name: NameTypes
+	name: NameTypes
 }
 export default function IconPark({ name, className, ...props }: PropsType) {
-  return (
-    <div className={`w-5 h-5 flex items-center justify-center ${className || ''}`} {...props}>
-      <iconpark-icon
-        style={{ fontSize: '20px', transition: 'color 0.02s' }}
-        name={name}
-      />
-    </div>
-  )
+	return (
+		<div className={`w-5 h-5 flex items-center justify-center ${className || ''}`} {...props}>
+			<iconpark-icon style={{ fontSize: '20px', transition: 'color 0.02s' }} name={name} />
+		</div>
+	)
 }
